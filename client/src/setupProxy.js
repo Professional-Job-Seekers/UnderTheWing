3bafa826e48da3044b37cde2383ed1ad8625a690
jacgit18 <joshuacarpentier21@gmail.com>
2020-10-31 17:15:31 -0400

    fixed docker client package-lock.json issue

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 let target_server = process.env.PROXY ||  "http://localhost:8080/";
-
+console.log(target_server);
 module.exports = app => {
-  app.use("/api/*", createProxyMiddleware({ target: target_server ,changeOrigin: true}));
+  app.use("/api/*", createProxyMiddleware({ target: target_server, changeOrigin: true}));
 }

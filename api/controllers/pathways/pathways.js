82302eff4c15b7f8012edafe5a940a47a86c676e
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-11-08 22:53:13 -0500

    Added additional controllers for pathways

--- api/controllers/pathways/pathways.js
+++ api/controllers/pathways/pathways.js
@@ -3,74 +3,39 @@ const router = express.Router();
 const pathwayQueries = require('./queries');
 
 /***************************************************************************************************
- ***************************************** Pathway Routing *****************************************
+ ******************************************* Re-Routing ********************************************
+ ***************************************************************************************************/
+
+const creationController = require('./creationController');
+const progressController = require('./progressController');
+const commitController = require('./commitController');
+const updateController = require('./updateController');
+router.use('/create', creationController);
+router.use('/commit', commitController);
+router.use('/update', updateController);
+router.use('/progress', progressController);
+
+
+ /***************************************************************************************************
+ ***************************************** Pathway Search ******************************************
  ***************************************************************************************************/
 
 router.get('/', async (req, res)=>{
-    const pathway_title = req.query.pathway;
-    let pathway = null;
-    try{
-        pathway = await pathwayQueries.findPathway(pathway_title);
-        if (!pathway){
-            res.status(404).json({"msg": "Pathway not found !"});
-            return;
-        }
-    } catch(err){
-        console.log(err);
-        res.sendStatus(500);
+  const pathway_title = req.query.pathway;
+  let pathway = null;
+  try{
+    pathway = await pathwayQueries.findPathway(pathway_title);
+    if (!pathway){
+        res.status(404).json({"msg": "Pathway not found !"});
+        return;
     }
-    res.status(200).json(pathway);
+  } catch(err){
+      console.log(err);
+      res.sendStatus(500);
+  }
+  res.status(200).json(pathway);
 });
 
-
-router.post('/create', async (req, res, next) =>{
-    if(req.body.tasks){
-        return next();
-    }
-    const pathway = {
-        "title" : req.body.title,
-    };
-    let new_pathway = null; 
-    try{
-        new_pathway = await pathwayQueries.createPathway(pathway)
-        res.status(200).json(new_pathway);
-    } catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-router.post('/create', async (req, res, next) =>{
-    if(req.body.categories){
-        return next();
-    }
-    const pathway = req.body.title;
-    const tasks =  req.body.tasks;
-    let newPathway = null; 
-    try{
-        newPathway = await pathwayQueries.createPathwayWithTasks(pathway, tasks);
-        console.log(newPathway);
-        res.status(200).json(newPathway);
-    }catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-router.post('/create', async (req, res) =>{
-    const pathway = req.body.title;
-    const tasks =  req.body.tasks;
-    const categories =  req.body.categories;
-    let newPathway = null; 
-    try{
-        newPathway = await pathwayQueries.createPathwayWithTasksAndCategories(pathway, tasks, categories);
-        console.log(newPathway);
-        res.status(200).json(newPathway);
-    }catch(err){
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
 
 
 module.exports = router;

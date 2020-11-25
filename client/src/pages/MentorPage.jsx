f17cd2f1fa2b6d681ec89b0af70f97f844c77cf1
jacgit18 <joshuacarpentier21@gmail.com>
2020-11-25 17:11:54 -0500

    push

--- client/src/pages/MentorPage.jsx
+++ client/src/pages/MentorPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import '../styles/App.css'
-import { Card, Col, Row, Container } from "react-bootstrap";
+import { Card, Col, Row, Container, Accordion,  Button } from "react-bootstrap";
 
 
 export default class MentorPage extends Component {
@@ -14,7 +14,23 @@ export default class MentorPage extends Component {
             <p>fsifjio</p>
             </Card>
           </Col>
+          <Col xs={6} className="d-flex flex-column">
+            <Accordion defaultActiveKey="0">
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                    Click me!
+                  </Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
         </Row>
+
+        
       </Container>
      
     );

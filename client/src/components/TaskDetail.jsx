e084448121f06caea460e08733e76ca70676c890
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-11-28 01:53:19 -0500

    Completed basic user dashboard

---
+++ client/src/components/TaskDetail.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default class TaskDetail extends React.Component {
+    constructor(props) {
+    }
+    getRelatedUsers(){
+    }
+    render(){
+        return ( 
+            <div>
+            </div>   
+        )
+    }
+}
--- client/src/pages/PathwayCreatorPage.jsx
+++ client/src/pages/PathwayCreatorPage.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import '../styles/App.css'
 import '../components/forms/PathwayCreationForm'
-import pathwayService from '../services/pathway'
 import PathwayCreationForm from '../components/forms/PathwayCreationForm';
 export default class PathwayCreatorPage extends React.Component {
-  state = {
-    success: false,
-    successMsg: "",
-    failureMsg:""    
-  }
-  async componentDidMount(){
-  }
   render(){
     return (
         <PathwayCreationForm/>

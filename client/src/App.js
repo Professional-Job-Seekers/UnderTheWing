5ca42b7f829ba7d62c02fc31c0ec979c9ecc6454
jacgit18 <joshuacarpentier21@gmail.com>
2020-12-02 18:03:29 -0500

    add style changes

--- client/src/App.js
+++ client/src/App.js
@@ -63,7 +63,7 @@ export default class App extends React.Component {
 
             </Router>
           </div>
-          <Footer className="Footer" />
+          {/* <Footer className="Footer" /> */}
         </>
       </main>
     );

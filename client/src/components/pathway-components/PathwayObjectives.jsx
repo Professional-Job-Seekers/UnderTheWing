db17859793128fc3fb8c3a9d90500eed378bbdeb
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-12-04 21:13:13 -0500

    Updated dashboard to map in componentDidMount instead of mapping in render function

--- client/src/components/pathway-components/PathwayObjectives.jsx
+++ client/src/components/pathway-components/PathwayObjectives.jsx
@@ -6,30 +6,45 @@ import PathwayDashboardTask from "./PathwayDashboardTask";
 import { Tabs, Tab, Container, Row, Col } from "react-bootstrap";
 
 export default class PathwayObjectives extends React.Component {
+  state = {
+    "activePathwayComponents": []
+  }
+
+  async componentDidMount() {
+    if(this.props.activePathwayData === null){
+      console.log(this.props.activePathwayData);
+      this.setState({
+        "activePathwayComponents": [],
+      });
+      return;
+    }
+    const activePathwayComponents = this.props.activePathwayData.map((pathway, index) => (
+      <Tab className="text-warning mt-5" eventKey={pathway.pathway} title={pathway.pathway} key={index} >
+        {pathway.tasks.map((task, index) => (
+          <div >
+            <PathwayDashboardTask
+              key={index}
+              taskTitle={task.title}
+              taskId={task.id}
+              baseURL={this.props.baseURL}
+            />
+          </div>
+        ))}
+      </Tab>
+    ));
+    this.setState({
+      "activePathwayComponents": activePathwayComponents,
+    });
+  }
+
   render() {
-    const activePathwayComponents = this.props.activePathwayData.map(
-      (pathway, index) => (
-        <Tab className="text-warning mt-5" eventKey={pathway.pathway} title={pathway.pathway} key={index} >
-          {pathway.tasks.map((task, index) => (
-            <div >
-              <PathwayDashboardTask
-                key={index}
-                taskTitle={task.title}
-                taskId={task.id}
-                baseURL={this.props.baseURL}
-              />
-            </div>
-          ))}
-        </Tab>
-      )
-    );
     return (
       <div>
         <Container className="cardbodycolor">
           <h2 className="text-light p-3">Your Active Pathways</h2>
           <Row>
             <Col>
-              <Tabs className=" mynavback btn-outline-warning  nav-fill"> {activePathwayComponents} </Tabs>
+              <Tabs className=" mynavback btn-outline-warning  nav-fill"> {this.state.activePathwayComponents} </Tabs>
             </Col>
           </Row>
         </Container>
